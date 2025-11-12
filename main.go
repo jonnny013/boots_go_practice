6975836a3a42efb87b3ee41793c04c7f793ccdaa
Jon Love <jon_w-l@hotmail.com>
2025-11-12 21:27:12 +0800

    Finish buffered channels

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 func addEmailsToQueue(emails []string) chan string {
-	// ?
+	ch := make(chan string, len(emails))
+
+	for i := range emails {
+		ch <- emails[i]
+	}
+
+	return ch
 }
 
 // https://www.boot.dev/lessons/d2a10614-3142-4d3e-906e-5a817aa920b3
