60627dd9b1d731de195e9da3b9ecd9cbb2897525
Jon Love <jon_w-l@hotmail.com>
2025-11-12 12:23:16 +0800

    prepare next lessons

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import "fmt"
-
-func waitForDBs(numDBs int, dbChan chan struct{}) {
-	for i := 0; i < numDBs; i++ {
-		<-dbChan
-	}
-
+func addEmailsToQueue(emails []string) chan string {
+	// ?
 }
 
-// don't touch below this line
-
-func getDBsChannel(numDBs int) (chan struct{}, *int) {
-	count := 0
-	ch := make(chan struct{})
-
-	go func() {
-		for i := 0; i < numDBs; i++ {
-			ch <- struct{}{}
-			fmt.Printf("Database %v is online\n", i+1)
-			count++
-		}
-	}()
-
-	return ch, &count
-}
+// https://www.boot.dev/lessons/d2a10614-3142-4d3e-906e-5a817aa920b3
