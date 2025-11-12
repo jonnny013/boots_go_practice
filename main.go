13fd52081aa57514cdabd4d377d4b85a79f42eac
Jon Love <jon_w-l@hotmail.com>
2025-11-12 11:58:10 +0800

    channels

--- main.go
+++ main.go
@@ -5,27 +5,54 @@ import (
 	"time"
 )
 
-func sendEmail(message string) {
-	 func() {
-		time.Sleep(time.Millisecond * 250)
-		fmt.Printf("Email received: '%s'\n", message)
-	}()
-	fmt.Printf("Email sent: '%s'\n", message)
+type email struct {
+	body string
+	date time.Time
 }
 
-// Don't touch below this line
 
-func test(message string) {
-	sendEmail(message)
-	time.Sleep(time.Millisecond * 500)
-	fmt.Println("========================")
-}
+
 
 func main() {
-	test("Hello there Kaladin!")
-	test("Hi there Shallan!")
-	test("Hey there Dalinar!")
+		runCases :=
+		[3]email{
+			{
+				body: "Words are pale shadows of forgotten names. As names have power, words have power.",
+				date: time.Date(2019, 2, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "It's like everyone tells a story about themselves inside their own head.",
+				date: time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "Bones mend. Regret stays with you forever.",
+				date: time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC),
+			},
+		}
+	result := checkEmailAge(runCases)
+	fmt.Println(result)
 }
 
+func checkEmailAge(emails [3]email) [3]bool {
+	isOldChan := make(chan bool)
 
-// next: https://www.boot.dev/lessons/04984711-09c4-4020-ac36-6d4214012d34
+	go sendIsOld(isOldChan, emails)
+
+	isOld := [3]bool{}
+	isOld[0] = <-isOldChan
+	isOld[1] = <-isOldChan
+	isOld[2] = <-isOldChan
+	return isOld
+}
+
+// don't touch below this line
+
+func sendIsOld(isOldChan chan<- bool, emails [3]email) {
+	for _, e := range emails {
+		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+			isOldChan <- true
+			continue
+		}
+		isOldChan <- false
+	}
+}
