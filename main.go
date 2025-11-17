124c67c262c9878100f137a89ab1cde9c5eb6b5b
Jon Love <jon_w-l@hotmail.com>
2025-11-17 09:59:46 +0800

    iota

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
+type emailStatus int
 
-func (a *analytics) handleEmailBounce(em email) error {
-	err := em.recipient.updateStatus(em.status)
-	if err != nil {
-		return fmt.Errorf("error updating user status: %w", err)
-	}
-	err = a.track(em.status)
-	if err != nil {
-		return fmt.Errorf("error tracking user bounce: %w", err)
-	}
-	return nil
-}
+const (
+	emailBounced emailStatus = iota
+	emailInvalid
+	emailDelivered
+	emailOpened
+)
